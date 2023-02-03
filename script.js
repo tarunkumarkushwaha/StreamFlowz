615d281e9425cbd2b387e9f82fd2536d5dd98683
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-02-03 17:42:12 +0530

    minor changes

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ let home = document.getElementById("home")
 let priceplan = document.getElementById("priceplan")
 let loginbox = document.getElementById("login-box")
 let bannertitle = document.getElementById("banner-title")
+let playlist = document.getElementById("playlist")
 
-const signupopener = ()=>{
-    bannertitle.setAttribute("class","invisible")
-    loginbox.removeAttribute("class","invisible")
-    priceplan.setAttribute("class","invisible")
+const signupopener = () => {
+    bannertitle.setAttribute("class", "invisible")
+    loginbox.removeAttribute("class", "invisible")
+    priceplan.setAttribute("class", "invisible")
+    playlist.setAttribute("class", "invisible")
 }
-const homeopener = ()=>{
+const homeopener = () => {
     window.location.reload()
 }
 
-const planopener = () =>{
-    bannertitle.setAttribute("class","invisible")
-    loginbox.setAttribute("class","invisible")
-    priceplan.removeAttribute("class","invisible")
+const planopener = () => {
+    bannertitle.setAttribute("class", "invisible")
+    loginbox.setAttribute("class", "invisible")
+    priceplan.removeAttribute("class", "invisible")
+    playlist.setAttribute("class", "invisible")
 }
 
-const playlistopener =()=>{
-    loginbox.setAttribute("class","invisible")
-    priceplan.setAttribute("class","invisible")
-    bannertitle.setAttribute("class","invisible")
-}
+const playlistopener = () => {
+        playlist.removeAttribute("class", "invisible")
+        loginbox.setAttribute("class", "invisible")
+        priceplan.setAttribute("class", "invisible")
+        bannertitle.setAttribute("class", "invisible")
+    }
+setTimeout(()=>{playlistopener()},3000)
